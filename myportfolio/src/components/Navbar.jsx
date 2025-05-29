408d5c78e5512479ce26a5e30c6970c5f47ad8e5
Ashan Ekanayake <ashanekanayakeat@gmail.com>
2025-05-29 09:44:05 +0530

    add navbar and add CSS to it

--- myportfolio/src/components/Navbar.jsx
+++ myportfolio/src/components/Navbar.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import '../styles/navbar.css';
+import React from "react";
+import "../styles/navbar.css";
+import { Link } from "react-router-dom";
+
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>ssss</h1>
-    </div>
-  )
-}
+    <nav className="navbar">
+      <img src="./src/assets/mypic.png" alt="logo" className="logo" />
+      <div className="desktopMenu">
+        <Link to='/' className="desktopMenuListItem">Home</Link>
+        <Link to='client'  className="desktopMenuListItem">Clients</Link>
+        <Link to='/about'  className="desktopMenuListItem">About</Link>
+        <Link to='/portfolio'  className="desktopMenuListItem">Portfolio</Link>
+      </div>
+      <button className="desktopMenuBtn">
+        Contact Me
+      </button>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
