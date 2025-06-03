ceb55abca1b0f12905cc2b4283f240b442954852
Ashan Ekanayake <ashanekanayakeat@gmail.com>
2025-06-03 20:21:25 +0530

    created works component

--- myportfolio/src/components/Works.jsx
+++ myportfolio/src/components/Works.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import '../styles/works.css';
+import React from "react";
+import "../styles/works.css";
 
 const Works = () => {
   return (
-    <section id='works'>
-        <h2 className='workstitle'>
-            My Portfolio
-        </h2>
+    <section id="works">
+      <h2 className="workstitle">My Portfolio</h2>
+      <span className="worksDesc">I take pride in attention to the smallest details and</span>
+      <div className="worksImgs">
+        <img src="./src/assets/works/portfolioimg1.jpg" alt="work1" className="worksImg"/>
+        <img src="./src/assets/works/portfolioimg1.jpg" alt="work1" className="worksImg"/>
+        <img src="./src/assets/works/portfolioimg1.jpg" alt="work1" className="worksImg"/>
+        <img src="./src/assets/works/portfolioimg1.jpg" alt="work1" className="worksImg"/>
+        <img src="./src/assets/works/portfolioimg1.jpg" alt="work1" className="worksImg"/>
+        <img src="./src/assets/works/portfolioimg1.jpg" alt="work1" className="worksImg"/>
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export default Works;
