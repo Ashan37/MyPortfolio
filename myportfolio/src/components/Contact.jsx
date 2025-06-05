cf7b889feef5bd59e3a91949a4d610a2f7abc4e1
Ashan Ekanayake <ashanekanayakeat@gmail.com>
2025-06-05 22:34:45 +0530

    Add links to contact page

--- myportfolio/src/components/Contact.jsx
+++ myportfolio/src/components/Contact.jsx
@@ -1,27 +1,61 @@
-import React from 'react'
-import '../styles/contact.css'
+import React from "react";
+import "../styles/contact.css";
+import { Link } from "react-router-dom";
 
 const Contact = () => {
   return (
-  <section id='contactPage'>
-    <div id="contact">
+    <section id="contactPage">
+      <div id="contact">
         <h1 className="contactPagetitle">Contact Me</h1>
-        <span className='contactDesc'>Please fill out form below to discuss any work oppertunities.</span>
+        <span className="contactDesc">
+          Please fill out form below to discuss any work oppertunities.
+        </span>
         <form className="contactForm">
-            <input type="text" className='name' placeholder='Your Name'/>
-            <input type="email" className='email' placeholder='Your Email'/>
-            <textarea className='msg' name="message" rows={5} placeholder='Your Message'></textarea>
-            <button type='submit'  className="submitBtn">Submit</button>
-            <div className="links">
-                <img src="./src/assets/contacts/facebook.png" alt="facebook" className="link" />
-                <img src="./src/assets/contacts/instagram.png" alt="instagram" className="link" />
-                <img src="./src/assets/contacts/linkedin.png" alt="linkedin" className="link" />
-                <img src="./src/assets/contacts/facebook.png" alt="facebook" className="link" />
-            </div>
+          <input type="text" className="name" placeholder="Your Name" />
+          <input type="email" className="email" placeholder="Your Email" />
+          <textarea
+            className="msg"
+            name="message"
+            rows={5}
+            placeholder="Your Message"
+          ></textarea>
+          <button type="submit" className="submitBtn">
+            Submit
+          </button>
+          <div className="links">
+            <Link>
+              <img
+                src="./src/assets/contacts/facebook.png"
+                alt="facebook"
+                className="link"
+              />
+            </Link>
+            <Link>
+              <img
+                src="./src/assets/contacts/instagram.png"
+                alt="instagram"
+                className="link"
+              />
+            </Link>
+            <Link>
+              <img
+                src="./src/assets/contacts/linkedin.png"
+                alt="linkedin"
+                className="link"
+              />
+            </Link>
+            <Link>
+              <img
+                src="./src/assets/contacts/facebook.png"
+                alt="facebook"
+                className="link"
+              />
+            </Link>
+          </div>
         </form>
-        </div>
-  </section>
-  )
-}
+      </div>
+    </section>
+  );
+};
 
-export default Contact
+export default Contact;
